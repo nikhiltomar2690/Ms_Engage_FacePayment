f5ca9c8422304def2404cfa10e863e39e37bcf60
Harshit Asthana <illusionw4@gmail.com>
2022-05-14 02:55:40 +0530

    Hello Face App

---
+++ app/src/main/java/com/example/msengage/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.msengage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.msengage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnget.setOnClickListener {
+            val intent = Intent(this@MainActivity, login::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
