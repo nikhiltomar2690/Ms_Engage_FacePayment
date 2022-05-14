10efc6a626d033c2eace52710720d414a3ebb1b6
Nikhil Tomar <nikhiltomar2690@gmail.com>
2022-05-15 02:27:36 +0530

    Otp func without Signup

--- app/src/main/java/com/example/msengage/login.kt
+++ app/src/main/java/com/example/msengage/login.kt
@@ -1,12 +1,87 @@
 package com.example.msengage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class login : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
+    lateinit var storedVerificationId:String
+    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        ja madarch
+        auth=FirebaseAuth.getInstance()
+
+        val btnotp = findViewById<Button>(R.id.btnotp)
+
+        var currentUser = auth.currentUser
+        if(currentUser != null) {
+            startActivity(Intent(applicationContext, home::class.java))
+            finish()
+        }
+        btnotp.setOnClickListener{
+            loginpage()
+        }
+        // Callback function for Phone Auth
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                startActivity(Intent(applicationContext, home::class.java))
+                finish()
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Toast.makeText(applicationContext, "Failed", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+
+                Log.d("TAG","onCodeSent:$verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+                var intent = Intent(applicationContext,otpenter::class.java)
+                intent.putExtra("storedVerificationId",storedVerificationId)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun loginpage() {
+        val mobileNumber=findViewById<EditText>(R.id.phoneno)
+        var number=mobileNumber.text.toString().trim()
+
+        if(!number.isEmpty()){
+            number="+91"+number
+            sendVerificationcode(number)
+        }else{
+            Toast.makeText(this,"Enter mobile number",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendVerificationcode(number: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(number) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
 }
